29bbc78bde45ff2a62df0403fda68a302404e8ed
Josh Murray <josh@joshmurray.eu>
2019-02-26 21:14:54 +0000

    Commit all of the outstanding work for the Books API actions and the BooksResource

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BookResource;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
@@ -12,23 +13,23 @@ class BooksController extends Controller
      *
      * @param Request $request
      * @param Book $book
-     * @return Book[]|\Illuminate\Database\Eloquent\Collection
+     * @return BookResource
      */
     public function index(Request $request, Book $book)
     {
-        // Get the fields of the model so we can check if the client is attemtping to filter
-        $fields = array_keys($book->getOriginal());
-        $dataFromRequest = $request->all($fields);
+        // Initialise some variables which will hold some request data
+        $author = $request->input('author');
 
-        // Is the client attempting to filter by columns that exist?
-        if (!empty($dataFromRequest)) {
-            // Yep, we have some filters.
-            $books = $book->where($dataFromRequest)->get();
+        if (!empty($author)) {
+            // we have an author.
+            $books = $book->whereHas('author', function ($query) use ($author) {
+                $query->where('name', 'like', "$author");
+            })->get();
         } else {
             // Nope, no filters - return all of the books.
             $books = $book->all();
         }
 
-        return $books;
+        return BookResource::collection($books);
     }
 }
